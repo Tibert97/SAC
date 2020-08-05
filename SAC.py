c54de19e97a7644e8d5da56d56c35c8706426d94
Durotan97 <40073323+Durotan97@users.noreply.github.com>
2020-08-05 15:01:59 +0200

    Update SAC.py

--- SAC.py
+++ SAC.py
@@ -6,6 +6,7 @@ import numpy as np
 import random
 import matplotlib
 import matplotlib.pyplot as plt
+%matplotlib inline
 import math
 import os
 import copy
@@ -14,6 +15,7 @@ import time
 import glob
 import io
 import pybullet
+import Box2D
 import pybullet_envs
 import base64
 import torch
@@ -31,8 +33,11 @@ from torch.utils import data
 import warnings
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'  # check whether a GPU is available
 
+from IPython import display as ipythondisplay
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(1400, 900))
+display.start()
 
-    
 class Agent(nn.Module):
     def __init__(self,d_state,d_action,action_limit):
         super(Agent,self).__init__()
@@ -76,6 +81,7 @@ class Agent(nn.Module):
 
         sampled_action = normal.rsample()
         log_prob = torch.sum(normal.log_prob(sampled_action),dim=1)
+        #correction of log prob based on spinning up implementation
         log_prob -= torch.sum((2*(np.log(2) - sampled_action - F.softplus(-2*sampled_action))),dim=1)
         
         return torch.tanh(sampled_action)*self.action_limit,log_prob,torch.tanh(mean)*self.action_limit
@@ -115,31 +121,59 @@ class A_Critic(nn.Module):
         x = self.lin2(x)
         x = self.norm2(x)
         x = F.relu(x)
-        
-        
     
         return (self.out(x))
+
+
 
 class ReplayBuffer(object):
   def __init__(self,capacity):
     self.capacity = capacity
     self.memory = list()
     self.position = 0
-    self.mins = list()
-    self.maxs = list()
+    self.changed_since_last_access = False
+    self.means = None
+    self.stds = None
   
   def add_sample(self,sample):
     if len(self.memory) < self.capacity:
       self.memory.append(None)
     self.memory[self.position] = torch.Tensor(sample).detach()
     self.position = (self.position + 1) % self.capacity
+    self.changed_since_last_access = True
+
 
   def get_sample(self, batch_size):
+    if self.changed_since_last_access:
+      self.recompute_statistics()
+
     samples = torch.stack(random.sample(self.memory, batch_size)).detach()
+    norm_samples = copy.deepcopy(samples)
+    for i in range(samples.size()[-1]):
+      norm_samples[:,i] -= self.means[i]
+      norm_samples[:,i] /= self.stds[i]
+    norm_samples[np.isnan(norm_samples)] = samples[np.isnan(norm_samples)]
     return samples
 
+  def recompute_statistics(self):
+    full_memory = torch.stack(self.memory)
+    self.means = torch.mean(full_memory,dim=0)
+    self.stds = torch.std(full_memory,dim=0)
+    self.changed_since_last_access = False
+
+  def normalise_observation(self,observation):
+    if self.changed_since_last_access:
+      self.recompute_statistics()
+
+    norm_observation = copy.deepcopy(observation)
+    for i in range(d_state):
+      norm_observation[:,i] -= self.means[i]
+      norm_observation[:,i] /= self.stds[i]
+    norm_observation[np.isnan(norm_observation)] = observation[np.isnan(norm_observation)]
+    return norm_observation
   def __len__(self):
     return len(self.memory)
+
 
 """
 Utility functions to enable video recording of gym environment and displaying it
@@ -151,7 +185,7 @@ To enable video, just do "env = wrap_env(env)""
 def show_video():
   mp4list = glob.glob('video/*.mp4')
   if len(mp4list) > 0:
-    mp4 = mp4list[-1]
+    mp4 = mp4list[0]
     video = io.open(mp4, 'r+b').read()
     encoded = base64.b64encode(video)
     ipythondisplay.display(HTML(data='''<video alt="test" autoplay 
@@ -170,11 +204,10 @@ def delete_videos():
   mp4list = glob.glob('video/*.mp4')
   for f in mp4list:
     os.remove(f)
-  
 
 def environment_step(environment,obs,step_counter):  
   with torch.no_grad():
-    agent_input = torch.Tensor(obs).view(1,-1).to(device)
+    agent_input = (torch.Tensor(obs).view(1,-1)).to(device)
     agent.eval()
     action,_,_ = agent(agent_input)
     agent.train()
@@ -188,10 +221,13 @@ def environment_step(environment,obs,step_counter):
       else:
         alive = 0
       show_video()
+      print(alive)
     else:
       alive = 1
     replay_buffer.add_sample(np.concatenate((obs[:d_state],action,observation[:d_state],[alive],[reward])))
     return observation,reward,done,step_counter
+
+
 
 def compute_q_targets(samples):
   s = samples[:,:d_state]
@@ -228,6 +264,8 @@ def update_q_network(targets,samples):
 
   q_a_losses_1.append(loss_1.item())
   q_a_losses_2.append(loss_2.item())
+
+
 
 
 def update_agent_network(samples):
@@ -326,10 +364,8 @@ def plot_policy_losses():
     ax2.set_ylabel("Agent Loss")
     plt.legend()
     plt.show()
-
-
-g_lr = 1e-4
-c_lr = 1e-4
+g_lr = 1e-3
+c_lr = 1e-3
 a_lr = 1e-3
 q_a_lr = 1e-3
 critic_steps = 5
@@ -340,19 +376,18 @@ LAMBDA = 10
 GAMMA = 0.99
 alpha_entropy = 0.2
 tau = 0.995
-agent_batches = 100
+agent_batches = 64
 batch_size_train = 64
 buffer = list()
-environment_name = 'HopperBulletEnv-v0'
+environment_name = 'Walker2DBulletEnv-v0'
 env = gym.make(environment_name)
 d_state = env.observation_space.shape[0]
 d_action = env.action_space.shape[0]
+d_noise = 1
 action_limit_max = env.action_space.high[0]
 action_limit_min = env.action_space.low[0]
 episode_length = env._max_episode_steps
 
-gen = Generator(d_state,d_action).to(device)
-crit = Critic(d_state,d_action).to(device)
 agent = Agent(d_state,d_action,action_limit_max).to(device)
 a_critic_1 = A_Critic(d_state,d_action).to(device)
 a_critic_target_1 = copy.deepcopy(a_critic_1)
@@ -360,10 +395,6 @@ a_critic_2 = A_Critic(d_state,d_action).to(device)
 a_critic_target_2 = copy.deepcopy(a_critic_2)
 
 replay_buffer = ReplayBuffer(1000000)
-c_overall_losses = list()
-c_real_losses = list()
-c_fake_losses = list()
-g_losses = list()
 a_losses_q = list()
 a_losses_entropy = list()
 a_losses_overall = list()
@@ -378,7 +409,7 @@ print(d_state)
 d_action = env.action_space.shape[0]
 episode_length = env._max_episode_steps
 print(d_action)
-for t in range(1000):
+while replay_buffer.__len__() < 500:
   start = env.reset()
   step_counter = 0
   observation = start
@@ -411,12 +442,11 @@ obs = env.reset()
 env.render()
 step_counter = 0
 cum_reward = 0
-plot_model_losses()
 episode_counter = 1
 for i in range(int(1e6)):
     obs,reward,done,step_counter = environment_step(env,obs,step_counter)
     cum_reward += reward
-    if i % 50 == 0 and i != 0:
+    if i % 50 == 0:
       agent_training(replay_buffer, epochs = 50)
     if done:
       print('Episode number',episode_counter)
@@ -428,5 +458,3 @@ for i in range(int(1e6)):
       step_counter = 0
       env.render()
       episode_counter += 1
-  
-  
